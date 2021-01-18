877300370754a895f8d7b67e9b7332d73bdaf216
mailspaghetti-admin <git@mailspaghetti.com>
2021-01-18 09:52:15 +0400

    renamed the solution to template-engine

--- src/test/java/com/mailspaghetti/templateengine/TemplateEngineApplicationTests.java
+++ src/test/java/com/mailspaghetti/templateengine/TemplateEngineApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PdfServiceApplicationTests {
+public class TemplateEngineApplicationTests {
 
 	@Test
 	public void contextLoads() {
